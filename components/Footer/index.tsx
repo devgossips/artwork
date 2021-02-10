4942540929aeab6ae1d412af24036a5e6e9099f8
Sagnik Sahoo <sahoosagnik1@gmail.com>
2021-02-10 14:39:01 +0530

    Update index.tsx

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -16,10 +16,10 @@ export default function Footer() {
               span: 5,
             }}
           >
-            <h5>Tabbied</h5>
+            <h5>ARTWORK</h5>
 
             <p>
-              Copyright 2020.{' '}
+              Copyright 2021.{' '}
               <Link href="/terms-of-service">
                 <a>Terms of Service</a>
               </Link>{' '}
@@ -42,14 +42,13 @@ export default function Footer() {
             <h5>About Us</h5>
 
             <p>
-              Tabbied lets you easily create timeless and beautifully generated
+              Artwork lets you easily create timeless and beautifully generated
               patterns or artwork to use for wall art, websites, print materials
               and more.
             </p>
             <p>
               This free tool was built by{' '}
-              <a href="https://www.syunghong.com/">Sy Hong</a> and{' '}
-              <a href="https://www.behance.net/yejoopark">Ye Joo Park</a>.
+              <a href="http://github.com/devgossips">Sagnik Sahoo</a>.
             </p>
           </Col>
         </Row>
